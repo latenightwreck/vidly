9012a4179925ce3d48eca6cfd23d76bb68e35f07
latenightwreck <morgan.b.armstrong@gmail.com>
2018-09-25 12:43:11 -0400

    Changed server close to await. combinded beforeEach

--- tests/integration/middleware/auth.test.js
+++ tests/integration/middleware/auth.test.js
@@ -4,24 +4,27 @@ const request = require('supertest')
 let server
 
 describe('auth middleware', () => {
-    beforeEach(() => { 
-      server = require('../../../index')
-      token = new User().generateAuthToken()
-    })
-    afterEach(async () => { server.close() })
 
-    let token
+  let token
 
-    const exec = () => {
-      return request(server)
-        .post('/api/genres')
-        .set('x-auth-token', token)
-        .send({ name: 'genre1' })
-    }
+  beforeEach(() => {
+    server = require('../../../index')
+    token = new User().generateAuthToken()
+  })
+  afterEach(async () => {
+    await server.close()
+  })
 
-    it('should return 401 if no token is provided', async () => {
-      token  = ''
-      const res = await exec()
-      expect(res.status).toBe(401)
-    })
+  const exec = () => {
+    return request(server)
+      .post('/api/genres')
+      .set('x-auth-token', token)
+      .send({ name: 'genre1' })
+  }
+
+  it('should return 401 if no token is provided', async () => {
+    token = ''
+    const res = await exec()
+    expect(res.status).toBe(401)
+  })
 })
