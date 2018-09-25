36e0700fc939cbab6b1732d1494a057bf481b53c
latenightwreck <morgan.b.armstrong@gmail.com>
2018-09-25 12:54:59 -0400

    remove genre after success test

--- tests/integration/middleware/auth.test.js
+++ tests/integration/middleware/auth.test.js
@@ -1,4 +1,5 @@
 const { User } = require('../../../models/user')
+const { Genre } = require('../../../models/genre')
 const request = require('supertest')
 
 let server
@@ -12,6 +13,7 @@ describe('auth middleware', () => {
     token = new User().generateAuthToken()
   })
   afterEach(async () => {
+    await Genre.remove({})
     await server.close()
   })
 
@@ -26,5 +28,16 @@ describe('auth middleware', () => {
     token = ''
     const res = await exec()
     expect(res.status).toBe(401)
+  })
+
+  it('should return 400 if token is invalid', async () => {
+    token = 'a'
+    const res = await exec()
+    expect(res.status).toBe(400)
+  })
+
+  it('should return 200 if token is valid', async () => {
+    const res = await exec()
+    expect(res.status).toBe(200)
   })
 })
